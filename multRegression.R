cf36e40f398ac7655dd14504fb4d7ecde7caf106
gracepenunuri <penunuri22@up.edu>
2022-01-25 12:37:25 -0800

    regressionScript2

--- multRegression.R
+++ multRegression.R
@@ -2,28 +2,44 @@ library(graphics)
 library(nnls)
 library(quadprog)
 #set data to matrix
-x<-as.matrix(allYR[])
+x<-as.matrix(comb23_45_67[])
 x<-t(x)
-x
+
+a<-as.matrix(comb23_67[])
+a<-t(a)
+
+b<-as.matrix(comb2_3[])
+b<-t(b)
 
 y<- as.matrix(eggNum[])
 y<-t(y)
-y
+
 
 nnls(x,y)
 
 #set matrix to a data.frame
-dx <- as.data.frame(x)
-dy<- as.data.frame(y)
+#dx <- as.data.frame(x)
+#dy<- as.data.frame(y)
 
 #mulitple regression function
 fit<- lm(y ~ 0+x)
 
+fitA<-lm(y~ 0+a)
+
+fitB <-lm(y~0+b)
+
 #details of lm
 anova(fit)
 summary(fit)
-
 predict(fit)
+
+anova(fitA)
+summary(fitA)
+predict(fitA)
+
+anova(fitB)
+summary(fitB)
+predict(fitB)
 
 
 #plotting 
